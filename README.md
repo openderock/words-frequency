85c8337619b166592af12b7f9a398a0edbb288c7
Sajjad shirazy <shirazy.sajjad@gmail.com>
2022-04-10 12:06:16 +0430

    Initialise

---
+++ README.md
@@ -0,0 +1,29 @@
+# Words frequency
+
+An NPM package for common English words frequency based on 20,000 [common words in Project Gutenberg](https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists#Project_Gutenberg).
+
+## Install
+
+```bash
+npm i @derockit/words-frequency
+```
+
+## Usage
+
+```javascript
+const wordsFrequency = require('@derockit/words-frequency');
+
+console.log(wordsFrequency[0]);
+// [1, "the", 5.6271872]
+```
+
+Every arry item includes:
+
+* Rank
+* Word
+* Occurrence percentage
+
+## CSV files
+
+* [The source file with 20,000 words](/resources/project-gutenberge-20000-common-words.csv).
+* [The cleaned list with 18,351 words](/dist/words-frequency.csv).
---
+++ index.js
@@ -0,0 +1,41 @@
+// @ts-check
+console.time('done');
+const fs = require('fs');
+
+const csvSource = fs.readFileSync(
+  './resources/project-gutenberge-20000-common-words.csv',
+  { encoding: 'utf8' }
+);
+
+const rows = csvSource.split('\n').map((row) => row.split(','));
+rows.shift();
+
+const cleanedWords = rows
+  .filter(([rank, word]) => /^[a-z]+$/.test(word))
+  .map(([rank, word, count], index) => [
+    index + 1,
+    word,
+    (parseInt(count) * 100) / Math.pow(10, 9),
+  ]);
+
+// saving as CSV
+const csvRows = cleanedWords.map((row) => row.join(','));
+csvRows.unshift('Rank,Word,Occurrence percentage');
+fs.writeFileSync('./dist/words-frequency.csv', csvRows.join('\n'), {
+  encoding: 'utf-8',
+});
+
+// saving as JS
+const jsArray = cleanedWords
+  .map(([rank, word, percentage]) => `[${rank},"${word}",${percentage}]`)
+  .join(',');
+fs.writeFileSync('./dist/words-frequency.js', `module.exports=[${jsArray}];`, {
+  encoding: 'utf-8',
+});
+
+// saving as JSON
+fs.writeFileSync('./dist/words-frequency.json', JSON.stringify(cleanedWords), {
+  encoding: 'utf-8',
+});
+
+console.timeEnd('done');
